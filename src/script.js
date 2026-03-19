b90236095762ee3c222da64c438cad63a96b27a9
clkaha <claire.hagger@norfolk.gov.uk>
2026-03-19 15:36:12 +0000

    Added JS to update city based on selector

--- src/script.js
+++ src/script.js
@@ -14,4 +14,24 @@ function updateTime() {
 	dublinTimeElement.innerHTML = `${moment().tz("Europe/Dublin").format(`h[:]mma`)}`;
 	dublinDateElement.innerHTML = `${moment().tz("Europe/Dublin").format(`DD MMMM YYYY`)}`;
 }
+
+function updateCity(event) {
+	let cityTimeZone = event.target.value;
+	let cityName = cityTimeZone.split("/")[1];
+	let cityTime = moment().tz(cityTimeZone);
+	let citiesContainer = document.querySelector("#cities-container");
+	citiesContainer.innerHTML = `<div class="city">
+					<div class="cityanddate">
+						<h2>${cityName}</h2>
+						<div class="date">${cityTime.format(`DD MMMM YYYY`)}</div>
+					</div>
+					<div class="time">${cityTime.format(`h[:]mma`)}</div>
+				</div>`;
+	console.log(cityTime.format(`h[:]mma`));
+}
+
+updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelector = document.querySelector("#cities-selector");
+citiesSelector.addEventListener("change", updateCity);
